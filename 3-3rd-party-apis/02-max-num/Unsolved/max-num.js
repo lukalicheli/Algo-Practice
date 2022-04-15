00d55021638de63bc97887aca4ece0ad81d6ca55
Luka Licheli <lukalicheli@gmail.com>
2022-04-15 13:49:38 -0700

    Finished max num algo

--- 3-3rd-party-apis/02-max-num/Unsolved/max-num.js
+++ 3-3rd-party-apis/02-max-num/Unsolved/max-num.js
@@ -1,3 +1,11 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function(array) {
+   let maxNum = array[0];
+   array.forEach(element => {
+       if (element > maxNum) {
+           maxNum = element;
+       }
+   });
+   return maxNum;
+};
